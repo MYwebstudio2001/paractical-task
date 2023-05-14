4c6f531dd85750fdec1c0955af9d57c3cf210e4e
kunjal <vadodakunjal.2001@gmail.com>
2023-05-14 12:37:37 +0530

    Summary view table

--- src/app/pages/summary/summary.component.ts
+++ src/app/pages/summary/summary.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import * as data from 'src/app/shared/data.json';
+
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent {
-  public dataSource: any;
+  public dataSource: any = data
 
-  constructor(private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.dataSource = data
-    console.log('this.dataSource :>> ', this.dataSource.Datas);
+  getProperty(item: any, label: string): any {
+    const prop = item.Properties.find((p: any) => p.Label === label);
+    return prop ? prop.Value : '';
   }
 }
 
